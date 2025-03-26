53236150da5a4c31ec4ba24b81fa47e0dd523d27
suillno <shtndlf46@gmail.com>
2025-03-26 15:06:42 +0900

    부족한부분 수정
    부족한부분 수정

--- src/finedustmain/Main.java
+++ src/finedustmain/Main.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -20,10 +23,10 @@ public class Main extends JFrame {
 	// https://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo?serviceKey=dfkqgFHSVFefp3CpeXZ5psT9s6iWXzaWacsy9b%2FgDvuEJpzyy9Vpah1z2ktIKXO85kIcRQAGZwdqUjUDbD6dkg%3D%3D&returnType=json&numOfRows=100&pageNo=1&year=2020&itemCode=PM10
 	private static final String API_KEY = "dfkqgFHSVFefp3CpeXZ5psT9s6iWXzaWacsy9b%2FgDvuEJpzyy9Vpah1z2ktIKXO85kIcRQAGZwdqUjUDbD6dkg%3D%3D";
 	private static final String BASE_URL = "https://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo";
-	
+	private static List<String> list = new ArrayList<String>();
 	
 	public static String getDustDate(String year) {
-		String urlstr = BASE_URL + "?serviceKey=" + API_KEY + "&returnType=json&numOfRows=1&pageNo=1&year=" + year;
+		String urlstr = BASE_URL + "?serviceKey=" + API_KEY + "&returnType=json&numOfRows=100&pageNo=1&year=" + year;
 				System.out.println(urlstr);
 		try {
 			URL url = new URL(urlstr);
@@ -45,30 +48,38 @@ public class Main extends JFrame {
 				in.close();
 				
 				String responseStr = response.toString();
-				System.out.println(responseStr);
+				
 				JSONObject obj = new JSONObject(responseStr.toString());
+				
 				JSONObject responseObj = obj.getJSONObject("response");
 				JSONObject bodyObj = responseObj.getJSONObject("body");
 				JSONArray itemsArray = bodyObj.getJSONArray("items");
-				System.out.println(itemsArray);
 				
-				String districtName = itemsArray.getJSONObject(0).getString("districtName");
-				String issueGbn = itemsArray.getJSONObject(0).getString("issueGbn");
-				String issueDate = itemsArray.getJSONObject(0).getString("issueDate");
-				String clearTime = itemsArray.getJSONObject(0).getString("clearTime");
-				double issueVale = itemsArray.getJSONObject(0).getDouble("issueVal");
+				for(int i = 0; i < itemsArray.length(); i++) {
+				String districtName = itemsArray.getJSONObject(i).getString("districtName");
+				String issueGbn = itemsArray.getJSONObject(i).getString("issueGbn");
+				String issueTime = itemsArray.getJSONObject(i).getString("issueTime");
+				String issueDate = itemsArray.getJSONObject(i).getString("issueDate");
+				String clearTime = itemsArray.getJSONObject(i).getString("clearTime");
+				double issueVale = itemsArray.getJSONObject(i).getDouble("issueVal");
 				
 				FineDustData dustData = new FineDustData();
 				dustData.setDistrictName(districtName);
 				dustData.setIssueGbn(issueGbn);
+				dustData.setIssueTime(issueTime);
 				dustData.setIssueDate(issueDate);
 				dustData.setClearTime(clearTime);
 				dustData.setIssueVale(issueVale);
 				
+				list.add(dustData.toString());
+				}
+				
+				if(list.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "해당 지역에 대한 경보 내역이 없습니다.", "오류", JOptionPane.ERROR_MESSAGE);
+				}
 				
 				
-				
-				return dustData.toString();
+				return list.toString();
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
