b2d687248ade8f7a40d30e8f1e570816476f5aac
suillno <shtndlf46@gmail.com>
2025-03-26 09:32:56 +0900

    미세먼지 확인 API
    팀 단위 준비

---
+++ FineDust/src/finedust/Frame.java
@@ -0,0 +1,10 @@
+package finedust;
+
+public class Frame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ FineDust/src/finedust/Main.java
@@ -0,0 +1,16 @@
+package finedust;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+	private static final String API_KEY = "dfkqgFHSVFefp3CpeXZ5psT9s6iWXzaWacsy9b%2FgDvuEJpzyy9Vpah1z2ktIKXO85kIcRQAGZwdqUjUDbD6dkg%3D%3D";
+	private static final String BASE_URL = "https://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo";
+	
+	
+
+	public static void main(String[] args) {
+		
+	}
+
+}
