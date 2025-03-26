fc3d247550ce25ebd8a6c0d935ce2ec8e5f6d9de
suillno <shtndlf46@gmail.com>
2025-03-26 14:15:56 +0900

    주석 추가
    주석 추가

--- src/finedustmain/DistrictMain.java
+++ src/finedustmain/DistrictMain.java
@@ -15,40 +15,55 @@ import org.json.JSONObject;
 import vo.FineDustData;
 
 public class DistrictMain {
+	//URL을 상수 설정 및 value를 담을 리스트 선언
 	private static final String API_KEY = "dfkqgFHSVFefp3CpeXZ5psT9s6iWXzaWacsy9b%2FgDvuEJpzyy9Vpah1z2ktIKXO85kIcRQAGZwdqUjUDbD6dkg%3D%3D";
 	private static final String BASE_URL = "https://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo";
 	private static List<String> list = new ArrayList<String>();
 	
+	// 검색조건 year과 값출력후 재검색 변수 district 설정(year 년도검색, district 도시검색)
 	public static String getDustDate(String year, String district) {
+		// year변수와 url 조합
 		String urlstr = BASE_URL + "?serviceKey=" + API_KEY + "&returnType=json&numOfRows=100&pageNo=1&year=" + year;
-				System.out.println(urlstr);
-		try {
+		
+		// URL 객체 생성후 Http 통신을위한 GET방식 연결설정
+		try { 
 			URL url = new URL(urlstr);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection(); 
 			conn.setRequestMethod("GET");
 			conn.setConnectTimeout(5000); // 연결시간 초과설정
 			conn.setReadTimeout(5000); // 읽기시간 초과설정
 			
+			// 서버 응답코드 출력
 			int responseCode = conn.getResponseCode();
 			
+			// 서버 응답코드 200ok시 진행
 			if (responseCode == HttpURLConnection.HTTP_OK) {
+				
+				// 서버응답을 바이트스트림으로 가져와 문자스트림으로 읽고 줄단위로 읽기위해 BufferedReader을 사용
 				BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 				String inputLine;
 				StringBuilder response = new StringBuilder();
 				
+				// inputLine과 in.readLine()가 null이 될때까지 스트링빌더를 통해 response에 문자열 이어붙이기
 				while ((inputLine = in.readLine()) != null) {
 					response.append(inputLine);
 				}
 				in.close();
 				
+				// 문자열을 이어붙인 response를 String 형태로 선언
 				String responseStr = response.toString();
-				System.out.println(responseStr);
+				
+				// String responseStr를 JSONObject 형태로 변경
 				JSONObject obj = new JSONObject(responseStr.toString());
+				
+				// 최상위 JSONObject에서 필요한 하위 객체와 배열 추출
 				JSONObject responseObj = obj.getJSONObject("response");
 				JSONObject bodyObj = responseObj.getJSONObject("body");
 				JSONArray itemsArray = bodyObj.getJSONArray("items");
 				System.out.println(itemsArray);
 				
+				// JitemsArray의 각 요소를 순회하며, district가 일치하면 FineDustData 객체에 값 저장 후 리스트에 추가
+				// 이후 해당 값을 list에 저장
 				for(int i = 0; i < itemsArray.length(); i++) {
 				String districtName = itemsArray.getJSONObject(i).getString("districtName");
 				String issueGbn = itemsArray.getJSONObject(i).getString("issueGbn");
@@ -67,9 +82,12 @@ public class DistrictMain {
 				list.add(dustData.toString());
 				}}
 			
+				// 일치하는 데이터가 없으면 오류 메시지 출력
 				if(list.isEmpty()) {
 					JOptionPane.showMessageDialog(null, "해당 지역에 대한 경보 내역이 없습니다.", "오류", JOptionPane.ERROR_MESSAGE);
 				}
+				
+				// ArrayList 리스트의 내용을 문자열로 변환하여 반환하여 리턴
 				return list.toString();
 			}
 		} catch (Exception e) {
