b22b383689afc95b9b057e1d89b840d099f1b03c
tpwhd3377 <tpwhd3377@gmail.com>
2025-03-26 10:19:06 +0900

    변수명 지정
    변수명 지정

--- src/finedustmain/Main.java
+++ src/finedustmain/Main.java
@@ -1,14 +1,21 @@
 package finedustmain;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Main extends JFrame {
 
 	private static final String API_KEY = "dfkqgFHSVFefp3CpeXZ5psT9s6iWXzaWacsy9b%2FgDvuEJpzyy9Vpah1z2ktIKXO85kIcRQAGZwdqUjUDbD6dkg%3D%3D";
 	private static final String BASE_URL = "https://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo";
 	
+	private JTextField yearText;
+	private JTextArea fineDustText;
 	
-
+	public static String getDustDate(int year) {
+		return null;
+	}
+	
 	public static void main(String[] args) {
 		
 	}
---
+++ src/vo/FineDustData.java
@@ -0,0 +1,12 @@
+package vo;
+
+public class FineDustData {
+	// 지역, 경보발령일, 경보해제일, 경보명
+	
+	String issueDate = ""; // 경보발생일
+	String clearTime = ""; // 경보해제일
+	String issueGbn = ""; // 경보명
+	String districtName = ""; // 지역
+	int issueVale; // 오염등급
+
+}
